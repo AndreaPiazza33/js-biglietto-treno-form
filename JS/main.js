fb0cb5f173795c56a9a94a4394afbb5a47757186
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-20 16:51:06 +0200

    feat: add conditions to user data

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,28 @@ const overageDiscount = (40 / 100)
 //variabile costo al km
 const kmPrice = (0.21)
 
+//condizioni numeri km e prezzo
+if (isNaN(passengerKm)){
+    alert(passengerKm + " non è un numero!")
+}
+else if (isNaN(passengerAge)){
+    alert(passengerAge + " non è un numero!")
+}
+
+if (passengerKm < 0){
+    alert(passengerKm + " non è un numero positivo!")
+}
+else if (passengerAge < 0){
+    alert(passengerAge + " non è un numero positivo!")
+}
+
+//condizioni per lo sconto
+if(passengerAge < 18){
+    x = underageDiscount
+}
+else if (passengerAge >= 65) {
+    x = overageDiscount
+}
+else {
+    x = 1
+}
