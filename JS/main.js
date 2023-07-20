a95584718b18f99d622ce96db2286ef72eb7fe7b
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-20 16:00:35 +0200

    feat: add js variables

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+//variabile km
+const  passengerKm = parseFloat(prompt ("quanti km percorrerai?"))
+
+//variabile età
+const passengerAge = parseInt(prompt("quanti anni hai?"))
+
+//variabile sconto minorenni
+const underageDiscount = (20 / 100)
+
+//variabile sconto over 65
+const overageDiscount = (40 / 100)
+
+//variabile costo al km
+const kmPrice = (0.21)
+
